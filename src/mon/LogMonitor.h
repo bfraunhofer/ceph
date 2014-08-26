6dd0f29b4c95d797577e65f3549a5b4190eb3fdd
Joao Eduardo Luis <joao.luis@inktank.com>
2014-08-26 16:45:17 +0100

    mon: LogMonitor: observe config changes
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

--- src/mon/LogMonitor.h
+++ src/mon/LogMonitor.h
@@ -30,7 +30,8 @@ class MMonCommand;
 
 static const string LOG_META_CHANNEL = "$channel";
 
-class LogMonitor : public PaxosService {
+class LogMonitor : public PaxosService,
+                   public md_config_obs_t {
 private:
   multimap<utime_t,LogEntry> pending_log;
   LogSummary pending_summary, summary;
@@ -149,5 +150,18 @@ private:
    */
   int sub_name_to_id(const string& n);
 
+  const char **get_tracked_conf_keys() const {
+    static const char* KEYS[] = {
+      "mon_cluster_log_to_syslog",
+      "mon_cluster_log_to_syslog_level",
+      "mon_cluster_log_to_syslog_facility",
+      "mon_cluster_log_file",
+      "mon_cluster_log_file_level",
+      NULL
+    };
+    return KEYS;
+  }
+  void handle_conf_change(const struct md_config_t *conf,
+                          const std::set<std::string> &changed);
 };
 #endif
